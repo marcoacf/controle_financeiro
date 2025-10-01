22b47af0e950f37ca3dcd8d215b069b3864bb9af
Marco Cruz <marcoacf@gmail.com>
2025-10-01 09:19:07 -0300

    Update 6_teste.py

--- pages/6_teste.py
+++ pages/6_teste.py
@@ -2,7 +2,19 @@ import os
 import json
 import streamlit as st
 # Verifica se está rodando com variável de ambiente (ex: Streamlit Cloud)
-str_cred = os.environ["GOOGLE_CREDENTIALS"]
-st.success(str_cred)
-str_json = json.loads(str_cred)
-st.success(str_json)
+#str_cred = os.environ["GOOGLE_CREDENTIALS"]
+#st.success(str_cred)
+#str_json = json.loads(str_cred)
+#st.success(str_json)
+
+# Lê credenciais do st.secrets
+    creds_dict = st.secrets["GOOGLE_CREDENTIALS"]
+
+    # Cria objeto de credenciais
+    creds = Credentials.from_service_account_info(
+        creds_dict,
+        scopes=["https://www.googleapis.com/auth/spreadsheets",
+                "https://www.googleapis.com/auth/drive"]
+    )
+
+    client = gspread.authorize(creds)

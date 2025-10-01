f135c57eb0b74eed44d2e8f7a3c82dcee20537f4
Marco Cruz <marcoacf@gmail.com>
2025-10-01 09:06:38 -0300

    Update 6_teste.py

--- pages/6_teste.py
+++ pages/6_teste.py
@@ -2,4 +2,7 @@ import os
 import json
 import streamlit as st
 # Verifica se está rodando com variável de ambiente (ex: Streamlit Cloud)
-st.success(os.environ["GOOGLE_CREDENTIALS"])
+str_cred = os.environ["GOOGLE_CREDENTIALS"]
+st.success(str_cred)
+str_json = json.loads(str_cred)
+st.success(str_json)

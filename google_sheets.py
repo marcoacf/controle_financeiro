548d6255251b22f9f7a339a3aa1d572d9efdba69
Marco Cruz <marcoacf@gmail.com>
2025-10-01 09:29:22 -0300

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import json
 from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2 import service_account as sa
 from config import GOOGLE_SHEET_ID, SHEET_NAME, CREDENTIALS_FILE
 
 def autenticar_google_sheets():
@@ -10,8 +11,10 @@ def autenticar_google_sheets():
              'https://www.googleapis.com/auth/drive']
 
     # Verifica se está rodando com variável de ambiente (ex: Streamlit Cloud)
-    creds = ServiceAccountCredentials.from_json_keyfile_name(json.loads(os.environ["GOOGLE_CREDENTIALS"]), scope)
-    
+    # Lê credenciais do st.secrets
+    creds_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
+    # creds = ServiceAccountCredentials.from_json_keyfile_name(json.loads(os.environ["GOOGLE_CREDENTIALS"]), scope)
+    creds = sa.Credentials.from_service_account_info(creds_dict, scope)
     client = gspread.authorize(creds)
     return client
 
@@ -25,6 +28,7 @@ def salvar_dado(novo_dado):
     client = autenticar_google_sheets()
     sheet = client.open_by_key(GOOGLE_SHEET_ID).worksheet(SHEET_NAME)
     sheet.append_row(novo_dado)
+
 
 
 
